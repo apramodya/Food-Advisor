641cf33a897f0e1addf29840994846f69e19dd9f
Pramodya <pramodya.emedia@gmail.com>
2020-12-20 08:48:10 +0530

    init project

---
+++ Food Advisor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Advisor
+//
+//  Created by Pramodya Abeysinghe on 2020-12-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
