31d10e6deeb6e45d0fdab67aafebecaf839d742f
Pramodya <pramodya.emedia@gmail.com>
2020-12-21 21:57:57 +0530

    create project structure

---
+++ Food Advisor/Views/Auth/RegistrationVC.swift
@@ -0,0 +1,29 @@
+//
+//  RegistrationVC.swift
+//  Food Advisor
+//
+//  Created by Pramodya Abeysinghe on 2020-12-21.
+//
+
+import UIKit
+
+class RegistrationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Food Advisor/Views/Restaurants/RestaurantsVC.swift
+++ Food Advisor/Views/Restaurants/RestaurantsVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RestaurantsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
